dd193fdf25d74cbd088e14f634b6344e06ad5fb4
Tim Miller <xxtimtimxx@gmail.com>
2023-07-06 02:35:28 -0400

    Added comparison for computer and player choices
    Added function that uses switch statements to compare the player's
    selection against the computer's selection to determine a winner. Also
    added variables for number of rounds, max score, player score, and
    computer score.

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
+const numOfRounds = 5
+const winningScore = 3
+let playerScore = 0
+let computerScore = 0
+
 // Get user input and ensure it is case insensitive
 function getPlayerChoice() {
   let playerChoice = prompt(
@@ -22,7 +27,61 @@ function getComputerChoice() {
   let computerChoice = choices[Math.floor(Math.random() * choices.length)]
   return computerChoice
 }
-console.log(getComputerChoice())
+
+// Compare player choice and computer choice
+function compareChoices(player, computer) {
+  let playerChoice = player
+  let computerChoice = computer
+  switch (playerChoice) {
+    case 'rock':
+      switch (computerChoice) {
+        case 'rock':
+          console.log('Both players chose Rock! This round is a draw!')
+          break
+        case 'paper':
+          console.log('Paper beats Rock! The computer wins this round!')
+          computerScore++
+          break
+        case 'scissors':
+          console.log('Rock beats scissors! The player wins this round!')
+          playerScore++
+          break
+      }
+      break
+    case 'paper':
+      switch (computerChoice) {
+        case 'rock':
+          console.log('Paper beats Rock! The player wins this round!')
+          playerScore++
+          break
+        case 'paper':
+          console.log('Both players chose Paper! This round is a draw!')
+          break
+        case 'scissors':
+          console.log('Scissors beats paper! The computer wins this round!')
+          computerScore++
+          break
+      }
+      break
+    case 'scissors':
+      switch (computerChoice) {
+        case 'rock':
+          console.log('Rock beats scissors! The computer wins this round!')
+          computerScore++
+          break
+        case 'paper':
+          console.log('Scissors beats paper! The player wins this round!')
+          playerScore++
+          break
+        case 'scissors':
+          console.log('Both players chose scissors! This round is a draw!')
+          computerScore++
+          break
+      }
+      break
+  }
+}
 // Display winner of each round
 // Keep track of score
 // Best of 5 rounds wins
+// Setup game
